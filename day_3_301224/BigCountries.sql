b2cd2fdcd0c4beea631731988e90581b2706d3d7
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2024-12-30 11:09:10 +0200

    Create BigCountries.sql

---
+++ day_3_301224/BigCountries.sql
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/big-countries/submissions/1492289286/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT name, population, area
+FROM world
+WHERE area >= 3000000 OR population >= 25000000
