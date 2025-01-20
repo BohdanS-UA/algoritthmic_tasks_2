8b76bd0717796016243dc1489d6a4c7e2d05dd9f
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-01-20 10:57:51 +0200

    Create OverTheRoad.java

---
+++ day_6_200125/OverTheRoad.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/java
+
+class CodeWars {
+  public static long overTheRoad(long address, long n) {
+    return  2 * n + 1 - address;
+  }
+}
