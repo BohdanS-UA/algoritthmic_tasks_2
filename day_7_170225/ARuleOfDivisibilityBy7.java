7e8db1b2ea21087f08782a66e781c03cf8dabeba
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-02-17 10:06:37 +0200

    Create ARuleOfDivisibilityBy7.java

---
+++ day_7_170225/ARuleOfDivisibilityBy7.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/55e6f5e58f7817808e00002e/train/java
+
+class DivSeven {
+    public static long[] seven(long m) {
+       String stringM = String.valueOf(m);
+        int steps = 0;
+        while (stringM.length() > 2) {
+            long decimal = m / 10;
+            long leftover = m - decimal * 10;
+            m = decimal - 2 * leftover;
+            stringM = String.valueOf(m);
+            steps++;
+        }
+        return new long[]{m, steps};
+    }
+}
