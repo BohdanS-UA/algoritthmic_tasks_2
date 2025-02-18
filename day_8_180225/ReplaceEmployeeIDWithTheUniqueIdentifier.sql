a8a4c7ffc7accf4b36fb1f37c66d68f2a1f7c042
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-02-18 11:26:07 +0200

    Create ReplaceEmployeeIDWithTheUniqueIdentifier.sql

---
+++ day_8_180225/ReplaceEmployeeIDWithTheUniqueIdentifier.sql
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT eu.unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI eu
+ON eu.id = e.id
