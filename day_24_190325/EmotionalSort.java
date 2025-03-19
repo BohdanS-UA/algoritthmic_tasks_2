a703dfecc80b7ef229c59d6b483c5b619b6f2338
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-03-19 09:53:22 +0200

    Create EmotionalSort.java

---
+++ day_24_190325/EmotionalSort.java
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/5a86073fb17101e453000258/train/java
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static String[] sortEmotions(boolean order, String[] emotions) {
+        Map<String, Integer> emotionMap = new HashMap<String, Integer>();
+        emotionMap.put(":D", 1);
+        emotionMap.put(":)", 2);
+        emotionMap.put(":|", 3);
+        emotionMap.put(":(", 4);
+        emotionMap.put("T_T", 5);
+
+        if (order) {
+            return Arrays.stream(emotions)
+                    .sorted(Comparator.comparing(emotion -> emotionMap.get(emotion)))
+                    .toArray(String[]::new);
+        } else {
+            return Arrays.stream(emotions)
+                    .sorted(Comparator.comparing(emotion -> emotionMap.get(emotion)).reversed())
+                    .toArray(String[]::new);
+        }
+    }
+}
