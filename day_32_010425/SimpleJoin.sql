a2af193e574b731c5009aed67ffefd6de46f006e
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-04-01 09:15:56 +0300

    Create SimpleJoin.sql

---
+++ day_32_010425/SimpleJoin.sql
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5802e32dd8c944e562000020/train/sql
+
+SELECT p.id, p.name, p.isbn, p.company_id, p.price, c.name AS company_name
+FROM products p
+LEFT JOIN companies c
+ON p.company_id = c.id
