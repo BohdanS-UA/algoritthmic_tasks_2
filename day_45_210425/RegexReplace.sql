67ae84e5a5145fce4f213fe0f519a2fd3a6c3d24
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-04-21 09:30:51 +0300

    Create RegexReplace.sql

---
+++ day_45_210425/RegexReplace.sql
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5942b066db68b6f35f000084/train/sql
+
+SELECT
+  project,
+  commits,
+  contributors,
+  REGEXP_REPLACE(address, '\d+', '!') AS address
+FROM repositories
