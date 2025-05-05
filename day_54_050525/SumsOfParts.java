c9eb09e12658e39fd6ea1a3cc69f2c7546da6364
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-05-05 09:05:45 +0300

    Create SumsOfParts.java

---
+++ day_54_050525/SumsOfParts.java
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/java
+
+import java.util.Arrays;
+
+class SumParts {
+  public static int[] sumParts(int[] ls) {
+        int[] result = new int[ls.length + 1];
+        result[0] = Arrays.stream(ls).sum();
+        for (int i = 1; i < ls.length; i++) {
+            result[i] = result[i - 1] - ls[i - 1];
+        }
+        result[ls.length] = 0;
+        return result;
+  }
+}
