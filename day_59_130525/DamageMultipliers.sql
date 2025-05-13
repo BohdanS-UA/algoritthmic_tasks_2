aa2169130f43ac486efeea677d8a069fc43089bb
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2025-05-13 09:01:38 +0300

    Create DamageMultipliers.sql

---
+++ day_59_130525/DamageMultipliers.sql
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5ab828bcedbcfc65ea000099/train/sql
+
+SELECT p.pokemon_name, 
+       (p.str * m.multiplier) AS modifiedStrength, 
+       m.element
+FROM pokemon p
+LEFT JOIN multipliers m ON p.element_id = m.id
+WHERE (p.str * m.multiplier) >= 40
+ORDER BY modifiedStrength DESC;
